a2be67c8ce6214c425ee424c127412a5c9db130d
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2024-09-25 18:21:59 -0700

    Separating game functionality from App.jsx into Game.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import Flag from './Flag.jsx';
 import AnswerBox from './AnswerBox.jsx';
 import Hint from './Hint.jsx';
